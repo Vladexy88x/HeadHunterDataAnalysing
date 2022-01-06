1d8bc12cb70a6de59388c1569b43cc8b8823f530
Vladexysys88 <jv69986998@gmail.com>
2022-01-06 15:34:44 +0300

    Update Item.cs

--- HeadHunterParser/Serialize/Item.cs
+++ HeadHunterParser/Serialize/Item.cs
@@ -1,10 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-
 namespace HeadHunterParser.Serialize
 {
     class Item
