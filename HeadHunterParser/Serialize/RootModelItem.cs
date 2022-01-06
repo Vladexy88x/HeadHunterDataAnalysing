414857e6426581e169bfce0fd8a2a9127407e9dc
Vladexysys88 <jv69986998@gmail.com>
2022-01-06 15:35:51 +0300

    Removed unnecessary directives

--- HeadHunterParser/Serialize/RootModelItem.cs
+++ HeadHunterParser/Serialize/RootModelItem.cs
@@ -1,10 +1,3 @@
-﻿using HeadHunterParser.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace HeadHunterParser.Serialize
 {
     class RootModelItem
