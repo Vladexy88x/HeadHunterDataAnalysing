a82130aee3f8e4fcc4e10664913fb11dcab8bf51
Vladexysys88 <jv69986998@gmail.com>
2022-01-06 15:35:08 +0300

    Removed unnecessary directives

--- HeadHunterParser/Serialize/Item.cs
+++ HeadHunterParser/Serialize/Item.cs
@@ -3,7 +3,7 @@ namespace HeadHunterParser.Serialize
     class Item
     {
          public string id { get; set; }
-         public string text { get; set; }
+         public string text { get; set;}
          public string url { get; set; }
     }
 }
